9b0c491a42094d537d9f24bb157ae76fedc59099
“ravinrawat” <ravinrawat.wd@gmail.com>
2024-03-24 11:55:01 -0600

    Added footer

---
+++ components/Footer.js
@@ -0,0 +1,15 @@
+const { View, StyleSheet } = require("react-native");
+
+const Footer = () => {
+  <View style={styles.wrapper}>
+    <Text>All rights reserved by Little Lemon, 2024</Text>
+  </View>;
+};
+
+const styles = StyleSheet.apply({
+  wrapper: {
+    backgroundColor: "yellow",
+    padding: 10,
+    alignItems: "center",
+  },
+});
