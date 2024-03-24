d8e471408d1bdd38010ca38811400b89a48defc2
“ravinrawat” <ravinrawat.wd@gmail.com>
2024-03-24 11:53:28 -0600

    Added header

---
+++ components/Header.js
@@ -0,0 +1,15 @@
+const { View, StyleSheet } = require("react-native");
+
+const Header = () => {
+  <View style={styles.wrapper}>
+    <Text>Little Lemon</Text>
+  </View>;
+};
+
+const styles = StyleSheet.apply({
+  wrapper: {
+    backgroundColor: "yellow",
+    padding: 10,
+    alignItems: "center",
+  },
+});
