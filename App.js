66737289ad4dc7f0fb1069a18d863cedb2664f77
“ravinrawat” <ravinrawat.wd@gmail.com>
2024-03-25 20:34:04 -0600

    Using useColorScheme for testing

--- App.js
+++ App.js
@@ -1,16 +1,35 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, SafeAreaView, Pressable } from "react-native";
+import {
+  StyleSheet,
+  SafeAreaView,
+  Pressable,
+  useWindowDimensions,
+  useColorScheme,
+} from "react-native";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import WelcomeScreen from "./screens/WelcomeScreen";
 import MenuItemsList from "./components/MenuItemsList";
 import FeedbackScreen from "./screens/FeedbackScreen";
 import LoginScreen from "./screens/LoginScreen";
+import {
+  useDeviceOrientation,
+  useAppState,
+} from "@react-native-community/hooks";
 
 const App = () => {
+  // const { width, height, fontScale } = useWindowDimensions();
+  // const orientation = useDeviceOrientation();
+  // const deviceStatus = useAppState();
+  const colorScheme = useColorScheme();
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView
+      style={[
+        styles.container,
+        { backgroundColor: colorScheme === "light" ? "#333333" : "#FFF" },
+      ]}
+    >
       <Header />
       {/* <FeedbackScreen /> */}
       {/* <LoginScreen /> */}
