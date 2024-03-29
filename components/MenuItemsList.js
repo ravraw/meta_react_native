14744a1e4fe61a44874a47c038731d2b1258715d
“ravinrawat” <ravinrawat.wd@gmail.com>
2024-03-29 15:55:47 -0600

    Code cleanup

--- components/MenuItemsList.js
+++ components/MenuItemsList.js
@@ -1,50 +1,13 @@
 import React, { useState } from "react";
-import { View, StyleSheet, SectionList, Text, Pressable } from "react-native";
+import {
+  View,
+  StyleSheet,
+  SectionList,
+  Text,
+  Pressable,
+  FlatList,
+} from "react-native";
 import MenuItem from "./MenuItem";
-
-const menuItemsToDisplay = [
-  {
-    title: "Appetizers",
-    data: [
-      { name: "Hummus", price: "$5.00" },
-      { name: "Moutabal", price: "$5.00" },
-      { name: "Falafel", price: "$7.50" },
-      { name: "Marinated Olives", price: "$5.00" },
-      { name: "Kofta", price: "$5.00" },
-      { name: "Eggplant Salad", price: "$8.50" },
-    ],
-  },
-  {
-    title: "Main Dishes",
-    data: [
-      { name: "Lentil Burger", price: "$10.00" },
-      { name: "Smoked Salmon", price: "$14.00" },
-      { name: "Kofta Burger", price: "$11.00" },
-      { name: "Turkish Kebab", price: "$15.50" },
-    ],
-  },
-  {
-    title: "Sides",
-    data: [
-      { name: "Fries", price: "$3.00", id: "11K" },
-      { name: "Buttered Rice", price: "$3.00" },
-      { name: "Bread Sticks", price: "$3.00" },
-      { name: "Pita Pocket", price: "$3.00" },
-      { name: "Lentil Soup", price: "$3.75" },
-      { name: "Greek Salad", price: "$6.00" },
-      { name: "Rice Pilaf", price: "$4.00" },
-    ],
-  },
-  {
-    title: "Desserts",
-    data: [
-      { name: "Baklava", price: "$3.00" },
-      { name: "Tartufo", price: "$3.00" },
-      { name: "Tiramisu", price: "$5.00" },
-      { name: "Panna Cotta", price: "$5.00" },
-    ],
-  },
-];
 
 const renderItem = ({ item: { name, price } }) => (
   <MenuItem name={name} price={price} />
@@ -69,7 +32,6 @@ const MenuItemsList = ({ menu }) => {
         )}
         ListFooterComponent={() => <Text>"--------- END -----------"</Text>}
       />
-      {/* )} */}
     </View>
   );
 };
--- screens/MenuScreen.js
+++ screens/MenuScreen.js
@@ -1,31 +1,33 @@
 import { useState, useEffect } from "react";
-import { View, Text } from "react-native";
+import { View, Text, ActivityIndicator } from "react-native";
 import MenuItemsList from "../components/MenuItemsList";
+import { menuItems } from "./helper/menuData";
 
 const MenuScreen = () => {
-  const [menu, setMenu] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [menu, setMenu] = useState(menuItems);
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
 
   const fetchMenu = async () => {
     try {
-      const response = await fetch("", {});
+      const response = await fetch(
+        "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu-items-by-category.json"
+      );
       const json = await response.json();
-      setMenu(json.menu);
-    } catch (e) {
-      console.error(e);
+      //   setMenu(json.menu);
+      setMenu(menuItems);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
     }
   };
+
   useEffect(() => {
     fetchMenu();
   }, []);
-  return (
-    <View>
-      {loading ? <Text> Loading .....</Text> : null}
-      {error ? <Text>{error}</Text> : null}
-      {!error && !loading && <MenuItemsList menu={menu} />}
-    </View>
-  );
+
+  return <MenuItemsList menu={menu} />;
 };
 
 export default MenuScreen;
