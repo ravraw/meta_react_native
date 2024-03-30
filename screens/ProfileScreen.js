491708c771952b8b91a232b4d2631fb8e1e2daf3
“ravinrawat” <ravinrawat.wd@gmail.com>
2024-03-30 15:33:19 -0600

    Updated Profile screen to include preferences

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Text, StyleSheet, View } from "react-native";
+import Constants from "expo-constants";
+import { Switch } from "react-native-paper";
 
 const ProfileScreen = ({ navigation }) => {
+  const defaultPreferences = {
+    pushNotifications: false,
+    emailMarketing: false,
+    latestNews: false,
+  };
+  const [preferences, setPreferences] = useState(defaultPreferences);
+  const updateState = (key) => () =>
+    setPreferences((prevState) => ({
+      ...prevState,
+      [key]: !prevState[key],
+    }));
+
   return (
     <View style={styles.container}>
-      <Text>Profile Screen</Text>
+      <Text style={styles.header}>Account Preferences</Text>
+      <View style={styles.row}>
+        <Text>Push notifications</Text>
+        <Switch
+          value={preferences.pushNotifications}
+          onValueChange={updateState("pushNotifications")}
+        />
+      </View>
+      <View style={styles.row}>
+        <Text>Marketing emails</Text>
+        <Switch
+          value={preferences.emailMarketing}
+          onValueChange={updateState("emailMarketing")}
+        />
+      </View>
+      <View style={styles.row}>
+        <Text>Latest news</Text>
+        <Switch
+          value={preferences.latestNews}
+          onValueChange={updateState("latestNews")}
+        />
+      </View>
     </View>
   );
 };
@@ -12,8 +47,20 @@ const ProfileScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: "#ecf0f1",
+    padding: 16,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 16,
+  },
+  header: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
 
