134f566cebac3079e439584bf92499bfebea3a2a
“ravinrawat” <ravinrawat.wd@gmail.com>
2024-04-10 20:45:20 -0600

    Added seconf footer screnn

---
+++ components/Footer2.js
@@ -0,0 +1,25 @@
+const { View, StyleSheet, Text } = require("react-native");
+
+const Footer = () => (
+  <View style={styles.wrapper}>
+    <Text style={styles.content}>
+      All rights reserved by Little Lemon @2024
+    </Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 0.06,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+  },
+  content: {
+    fontSize: 12,
+    color: "#4c6258",
+    fontWeight: "bold",
+  },
+});
+
+export default Footer;
